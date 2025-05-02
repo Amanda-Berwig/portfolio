0d292d0be0f125dfce625fe69256110ebb0bf2de
Amanda Berwig <amanda.berwig@gmail.com>
2025-05-02 20:14:26 -0300

    Ajusta cores e espaçamento do componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-purple-400 text-white shadow-md z-50">
-      <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <div className="text-xl font-bold">Portfolio</div>
-        <ul className="flex space-x-6 items-center">
+    <nav className="fixed top-0 left-0 text-lg w-full text-white shadow-md z-50">
+      <div className="container mx-auto px-4 py-3 flex justify-end items-center">
+        {/* <div className="text-xl font-bold">Portfolio</div> */}
+        <ul className="flex space-x-10 items-center">
           <li>
             <a
               href="#home"
-              className="hover:text-blue-400 transition duration-300"
+              className="hover:text-cyan-300 transition duration-300"
             >
               Home
             </a>
@@ -17,7 +17,7 @@ const Navbar = () => {
           <li>
             <a
               href="#about"
-              className="hover:text-blue-400 transition duration-300"
+              className="hover:text-cyan-300 transition duration-300"
             >
               Sobre
             </a>
@@ -25,7 +25,7 @@ const Navbar = () => {
           <li>
             <a
               href="#projects"
-              className="hover:text-blue-400 transition duration-300"
+              className="hover:text-cyan-300 transition duration-300"
             >
               Projetos
             </a>
@@ -33,7 +33,7 @@ const Navbar = () => {
           <li>
             <a
               href="#contact"
-              className="hover:text-blue-400 transition duration-300"
+              className="hover:text-cyan-300 transition duration-300"
             >
               Contato
             </a>
@@ -43,7 +43,7 @@ const Navbar = () => {
               href="/curriculo-amanda.pdf"
               target="_blank"
               rel="noopener noreferrer"
-              className="border border-purple-600 rounded py-2 px-4 hover:shadow-[3px_3px_0_0_rgba(128,0,128,1)] transform transition-all duration-200 ease-in-out"
+              className="border border-cyan-300 rounded py-2 px-4 hover:shadow-[3px_3px_0_0_rgba(103,232,249,1)] transform transition-all duration-200 ease-in-out"
             >
               Currículo
             </a>
