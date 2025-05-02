e2182c6b815cb50b68700e151ed8ca36e5c09b23
Amanda Berwig <amanda.berwig@gmail.com>
2025-05-01 22:35:15 -0300

    Feat: adiciona mais projetos a lista de projetos

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -2,6 +2,10 @@ import P01 from "../../assets/p01.png";
 import P02 from "../../assets/preview-todolist.png";
 import P03 from "../../assets/preview-organo.png";
 import P04 from "../../assets/preview-codeconnect.png";
+// import P05 from "../../assets/preview-avanti.png";
+import P06 from "../../assets/preview-fokus.png";
+import P07 from "../../assets/preview-jogo.png";
+import P08 from "../../assets/preview-alurabooks.png";
 
 export const projects = [
   {
@@ -30,7 +34,7 @@ export const projects = [
     description:
       "Painel para visualizar a estrutura hierárquica de sua equipe e auxiliar a gerir os colaboradores, separando-os por times.",
     image: P03,
-    tags: ["React", "D3.js", "Express"],
+    tags: ["React", "CSS"],
     github: "https://github.com/Amanda-Berwig/organo",
     demo: "https://organo-psi-three.vercel.app/",
   },
@@ -40,8 +44,47 @@ export const projects = [
     description:
       "Plataforma de blog com sistema de comentários, categorias e pesquisa avançada.",
     image: P04,
-    tags: ["React", "GraphQL", "Styled Components"],
+    tags: ["React", "CSS"],
     github: "https://github.com/Amanda-Berwig/Code-Connect",
     demo: "https://code-connect-react-vite.vercel.app/",
+  },
+  {
+    id: 5,
+    title: "Avanti Innovation Class",
+    description:
+      "Site responsivo com campo de busca interativo e carrosséis funcionais",
+    image: P04,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Amanda-Berwig/Avanti--innovation-class",
+    demo: "https://amanda-berwig.github.io/Avanti--innovation-class/",
+  },
+  {
+    id: 6,
+    title: "Fokus",
+    description:
+      "Temporizador com a técnica Pomodoro, com cronômetro e lista de tarefas.",
+    image: P06,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/Amanda-Berwig/Fokus",
+    demo: "https://fokus-two-zeta.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "Jogo do Número Secreto",
+    description:
+      "Aplicação simples desenvolvida com JavaScript puro, onde o usuário tenta adivinhar um número aleatório entre 1 e 10. Inclui feedback de voz com a biblioteca ResponsiveVoice, tornando a experiência mais interativa e acessível",
+    image: P07,
+    tags: ["HTML", "CSS", "JavaScript", "Responsive voice"],
+    github: "https://github.com/Amanda-Berwig/jogo-do-numero-secreto",
+    demo: "https://jogo-sage-psi.vercel.app/",
+  },
+  {
+    id: 8,
+    title: "Alura Books",
+    description: "E-commerce, uma livraria online de livros sobre tecnologia.",
+    image: P08,
+    tags: ["HTML", "CSS", "JavaScript", "Swiper JS"],
+    github: "https://github.com/Amanda-Berwig/AluraBooks",
+    demo: "https://alura-books-ashy-beta.vercel.app/",
   },
 ];
