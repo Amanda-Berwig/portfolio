6a6856fbf9dde865467dfc91ff1a60e9a429fee2
Amanda Berwig <amanda.berwig@gmail.com>
2025-05-05 14:04:58 -0300

    feat: Ajusta estilos do componente About para melhor responsividade

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <section id="about" className="py-16">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-12 text-slate-50">
+        <h2 className="text-3xl font-bold text-center mb-6 md:mb-12 text-slate-50">
           {translations.AboutMe}
         </h2>
         <div className="max-w-4xl mx-auto">
